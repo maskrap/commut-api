1246b9224e452ac40f9dce06430bfc708bf26ea9
Sam Park <sampark18@gmail.com>
2016-11-10 10:45:34 -0800

    Initial commit commutapi

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var GoogleMapsAPI = require('googlemaps');
+var app = express()
+
+app.get('/', function (req, res) {
+  var publicConfig = {
+    key: 'AIzaSyAqpWjz6H7emmTezZQsDs3aqcovG5fqm4w',
+    stagger_time:       1000, // for elevationPath
+    encode_polylines:   false,
+    secure:             true, // use https
+  };
+  var gmAPI = new GoogleMapsAPI(publicConfig);
+
+  var params = {
+          origin: 'Chicago, IL',
+          destination: 'Evanston, IL'
+        };
+
+  gmAPI.directions( params, function(err, result){
+    res.json({
+      "duration": result.routes[0].legs[0].duration.text
+    })
+  });
+})
+
+app.listen(3000, function () {
+  console.log('NSA is listening to port 3000!')
+})
